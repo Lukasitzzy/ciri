35d62ad2ca5575ab82e2024db4c977034a94c240
Lukas <70811215+Lukasitzzy@users.noreply.github.com>
2021-01-16 10:14:32 +0100

    more "custom" classes

---
+++ src/core/structures/inhibitors/Inhibitor.ts
@@ -0,0 +1,21 @@
+import { Inhibitor, InhibitorOptions } from "discord-akairo";
+import { DiscordBotClient } from "../../client/Client";
+
+
+export class CustomInhibitor extends Inhibitor {
+
+    public client!: DiscordBotClient;
+
+    /**
+     *
+     */
+    constructor({
+        id,
+        options
+    }: {
+        id: string;
+        options: InhibitorOptions;
+    }) {
+        super(id, options);
+    }
+}
---
+++ src/core/structures/listener/Listener.ts
@@ -0,0 +1,20 @@
+import { Listener, ListenerOptions } from "discord-akairo";
+import { DiscordBotClient } from "../../client/Client";
+
+
+export class CustomListener extends Listener {
+    public client!: DiscordBotClient;
+
+    /**
+     *
+     */
+    constructor({
+        id,
+        options
+    }: {
+        id: string;
+        options: ListenerOptions;
+    }) {
+        super(id, options);
+    }
+}
