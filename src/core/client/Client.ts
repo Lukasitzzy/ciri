ec81a2e91c879d29d0be530b23145962ddb4ef45
Lukas <70811215+Lukasitzzy@users.noreply.github.com>
2021-01-16 10:14:46 +0100

    added CommandHandler

--- src/core/client/Client.ts
+++ src/core/client/Client.ts
@@ -1,5 +1,6 @@
-import { AkairoClient } from 'discord-akairo';
+import { AkairoClient, CommandHandler } from 'discord-akairo';
 import { Intents } from 'discord.js';
+import { join } from 'path';
 
 /**
  * the client that connects to the discord Websocket
@@ -7,8 +8,9 @@ import { Intents } from 'discord.js';
  */
 export class DiscordBotClient extends AkairoClient {
 
+    public commandHandler: CommandHandler;
     /**
-     * 
+     * the root directory string 
      */
     private readonly root: string;
 
@@ -24,12 +26,21 @@ export class DiscordBotClient extends AkairoClient {
             }
         });
         this.root = ROOT;
+        this.commandHandler = new CommandHandler(this, {
+            directory: join(ROOT, 'commands'),
+            prefix: process.env.DISCORD_COMMAND_PREFIX || '$',
+            handleEdits: true,
+            commandUtil: true,
+            commandUtilLifetime: 3e5,
+            commandUtilSweepInterval: 3e5 + 1
+        });
     }
 
     /**
      * 
      */
     public async start(): Promise<void> {
+        this.commandHandler.loadAll();
         await this.login();
     }
 
