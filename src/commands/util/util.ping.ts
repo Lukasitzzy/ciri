4316778578635954e0d51ec080df8873f54e9e77
Lukas <70811215+Lukasitzzy@users.noreply.github.com>
2021-01-16 10:14:59 +0100

    Ping Command

---
+++ src/commands/util/util.ping.ts
@@ -0,0 +1,23 @@
+import { CustomCommand } from "../../core/structures/commands/Command";
+import { CommandContext } from "../../core/structures/commands/CommandContext";
+
+
+export default class PingCommand extends CustomCommand {
+    /**
+     *
+     */
+    constructor() {
+        super({
+            id: 'util.ping',
+            options: {
+                aliases: ['ping']
+            }
+        });
+    }
+    public async run(ctx: CommandContext<Record<string, unknown>>): Promise<void> {
+        const pingMessage = await ctx.say('pinging.....');
+        const clientWSPing = this.client.ws.ping;
+        await pingMessage.edit(`PONG! \`${clientWSPing}\`ms ( message \`${(pingMessage.editedTimestamp || pingMessage.createdTimestamp) - (ctx.msg.editedTimestamp || ctx.createdTimestamp)}\`ms )`);
+    }
+
+}
