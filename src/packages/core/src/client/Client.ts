990c0ee5032cdc88f09538a59d38b6aefbab9eb3
LukasItzzy <70811215+Lukasitzzy@users.noreply.github.com>
2021-02-13 16:55:52 +0100

    feat(package/core): added clien class

---
+++ src/packages/core/src/client/Client.ts
@@ -0,0 +1,33 @@
+import { AkairoClient } from 'discord-akairo';
+import { Intents } from 'discord.js';
+import { join } from 'node:path';
+import { CustomCommandHandler } from '../commands/CommandHandler';
+import { CustomCommand } from '../commands/CustomCommand';
+
+export class DiscordBot extends AkairoClient {
+
+    public commandHandler: CustomCommandHandler<CustomCommand>;
+
+    /**
+     *
+     */
+    constructor(root: string) {
+        super({
+            ownerID: process.env.OWNER_ID?.split('--'),
+            intents: Intents.ALL
+        });
+
+
+        this.commandHandler = new CustomCommandHandler(this, {
+            directory: join(root, 'commands'),
+            handleEdits: true,
+            commandUtil: true,
+            prefix: '$'
+        });
+
+
+    }
+    private _prepare(): void {
+        this.commandHandler.loadAll();
+    }
+}
