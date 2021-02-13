f1b3c7d0a95c82ce6f6b5828c8052a6a8369f9d6
LukasItzzy <70811215+Lukasitzzy@users.noreply.github.com>
2021-02-13 09:51:21 +0100

    feat(*): initial packages commit

---
+++ src/packages/Colors/src/index.ts
@@ -0,0 +1,30 @@
+import * as chalk from 'chalk';
+import *  as colorNames from 'color-name';
+
+
+export class ColorConverter {
+    constructor() {
+        throw new Error('not initialable');
+
+    }
+
+
+    public static hextoRgB(hex: string): colorNames.RGB {
+
+        const rgb: [number, number, number] = [0, 0, 0];
+
+        return rgb;
+    }
+
+    public static rgbToHex(rgb: colorNames.RGB): string {
+        const str = '';
+
+
+        return str;
+    }
+
+
+    public static hslToHex(hsl: any): string {
+        return '';
+    }
+}
