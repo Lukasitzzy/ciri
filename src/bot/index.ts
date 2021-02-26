34223f273306b467b132963b82af577a0ae6bc3c
Lukasitzzy <70811215+Lukasitzzy@users.noreply.github.com>
2021-02-26 08:07:19 +0100

    handle Promise rejections

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,3 +1,11 @@
 import { DiscordBot } from '../packages/core/src/client/Client';
 const client = new DiscordBot(__dirname);
 client.start().catch(e => client.logger.error(e));
+
+process.on('unhandledRejection', (res) => {
+    if (res instanceof Error) {
+        client.logger.error(res, 'Promise-rejection');
+    } else {
+        client.logger.error(new Error('promise rejection with no data'));
+    }
+});
