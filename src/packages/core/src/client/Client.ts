652f9efe0b65c67621cf418f9d8dee903f6e6a68
JastLucy <70811215+JastLucy@users.noreply.github.com>
2021-04-01 07:53:42 +0200

    re-ordered  handler loads

--- src/packages/core/src/client/Client.ts
+++ src/packages/core/src/client/Client.ts
@@ -81,16 +81,18 @@ export class DiscordBot extends AkairoClient {
 
     private _prepare(): void {
 
-        this.commandHandler
-            .useInhibitorHandler(this.inhibitorHandler)
-            .useListenerHandler(this.listenerHandler)
-            .loadAll();
         this.listenerHandler.setEmitters({
             client: this,
             commandHandler: this.commandHandler,
             ws: this.ws
         });
         this.listenerHandler.loadAll();
+        this.commandHandler
+            .useInhibitorHandler(this.inhibitorHandler)
+            .useListenerHandler(this.listenerHandler)
+            .loadAll();
+
+
         // this.inhibitorHandler.loadAll();
     }
 }
