270f94506a89ca47f63839d8b2ef82228ce5afec
JastLucy <70811215+JastLucy@users.noreply.github.com>
2021-04-03 09:27:05 +0200

    update help command

--- src/bot/slash_commands/help.ts
+++ src/bot/slash_commands/help.ts
@@ -1,17 +1,23 @@
 import { SlashCommand } from '../../packages/slash-commands/src/commands/SlashCommand';
 import { TextChannel } from 'discord.js';
+import { InteractionResponseType } from '../../packages/slash-commands/src/util/Constants';
 
 export default class HelpSlashCommand extends SlashCommand {
 
-    public run(): void | Promise<void> {
-        console.log(this.interaction.options);
-    }    
+    public async run(): Promise<void> {
+        await this.interaction.reply({
+            content: 'not yet here',
+            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+            ephemeral: true
+        });
+
+    }
 
     async userPermissions(): Promise<boolean> {
 
         return (this.interaction.member && this.interaction.channel?.isText() &&
             (this.interaction.channel as TextChannel)
-            .permissionsFor(this.interaction.member)
-            .has('SEND_MESSAGES')) || false;
+                .permissionsFor(this.interaction.member)
+                .has('SEND_MESSAGES')) || false;
     }
 }
