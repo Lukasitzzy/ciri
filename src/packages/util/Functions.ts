db79ad1a65f1a8224c614fb6927f676e57ef7ad5
JastLucy <70811215+JastLucy@users.noreply.github.com>
2021-04-12 19:06:07 +0200

    unused he,lper

--- src/packages/util/Functions.ts
+++ src/packages/util/Functions.ts
@@ -2,6 +2,7 @@ import { CommandOptions } from 'discord-akairo';
 import { DiscordBot } from '../core/src/client/Client';
 import { CustomCommand } from '../core/src/commands/CustomCommand';
 import { Api } from './typings/Discord.js.Api';
+import { exec } from 'child_process';
 
 export function getApi(client: DiscordBot): Api {
     return Reflect.get(client, 'api');
@@ -17,4 +18,19 @@ export function applyOptions(options: { id: string; description: any; options: C
 
         return Extended;
     };
+}
+
+
+export function getGitCommit(): Promise<string> {
+    return new Promise<string>((res, rej) => {
+        exec('git git rev-parse HEAD', (err, stdout, stderr) => {
+            if (err) {
+                return rej(err);
+            }
+            if (stdout) {
+                return res(stdout);
+            }
+            rej(new Error(stderr));
+        });
+    });
 }
